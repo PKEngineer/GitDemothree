55b3784fc6b16cc84c747d690139779e948a6eaa
PKEngineer <91558196+PKEngineer@users.noreply.github.com>
2021-09-30 21:36:34 -0400

    Update GitDemoThree.java
    pull changes n Xyz added

--- src/demothree/GitDemoThree.java
+++ src/demothree/GitDemoThree.java
@@ -10,6 +10,12 @@ public class GitDemoThree {
 				System.out.println("abc");
 			}else {
 				System.out.println("Xyz");
+				
+				// pull changes made in git
+				
+				int Xyz= 100;
+				int abc = 10;
+				
 			}
 		
 	}
