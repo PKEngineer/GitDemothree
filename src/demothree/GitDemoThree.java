87bb45a7f3191368d5c7c786a9fb282c2848ac49
user1 <user1@user.fios-router.home>
2021-09-30 21:29:32 -0400

    commit package three

---
+++ src/demothree/GitDemoThree.java
@@ -0,0 +1,16 @@
+package demothree;
+
+public class GitDemoThree {
+	public static void main(String[] args) {
+			System.out.println("DemoThree");
+			
+			boolean isSunday = true;
+			
+			if(isSunday) {
+				System.out.println("abc");
+			}else {
+				System.out.println("Xyz");
+			}
+		
+	}
+}
--- src/demotwo/GitDemoThree.java
+++
@@ -1,7 +0,0 @@
-package demotwo;
-
-public class GitDemoThree {
-	public static void main(String[] args) {
-		System.out.println("DemoThree");
-	}
-}
